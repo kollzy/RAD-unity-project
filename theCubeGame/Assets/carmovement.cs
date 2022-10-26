d1b8a7ad8af6e5a19c2c5d20fcb0d83409574ac8
STUDENT Kolawole Julius Kehinde <Kolawole.Julius.Kehinde@students.ittralee.ie>
2022-10-26 13:57:34 +0100

    Signed-off-by: STUDENT Kolawole Julius Kehinde <Kolawole.Julius.Kehinde@students.ittralee.ie>

--- theCubeGame/Assets/carmovement.cs
+++ theCubeGame/Assets/carmovement.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class carmovement : MonoBehaviour
-{
+{ 
+public float carspeed = 20;
+public float turningspeed = 50;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,13 @@ public class carmovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+    if (Input.GetKey(KeyCode.W))
+        transform.position += transform.forward * carspeed * Time.deltaTime;
+    if(Input.GetKey(KeyCode.S))
+        transform.position += -transform.forward * carspeed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+            transform.Rotate(Vector3.up, -turningspeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(Vector3.down, -turningspeed * Time.deltaTime);
     }
 }
