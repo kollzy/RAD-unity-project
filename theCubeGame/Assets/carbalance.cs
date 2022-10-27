81545d42e8eb1237910d501a5ed96921697dbf9e
julius kehinde <kolakehinde485@gmail.com>
2022-10-27 21:36:24 +0100

    Signed-off-by: julius kehinde <kolakehinde485@gmail.com>

---
+++ theCubeGame/Assets/carbalance.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carbalance : MonoBehaviour
+{
+	// i got this code from the internet and i modified it a bit
+	// Start is called before the first frame update
+
+
+
+	public WheelCollider WheelL;
+	public WheelCollider WheelR;
+	public float balance = 5000.0f;
+
+	private Rigidbody car;
+
+	void Start()
+	{
+		car = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate()
+	{
+		WheelHit hit;
+		float travelL = 1.0f;
+		float travelR = 1.0f;
+
+
+		bool groundedL = WheelL.GetGroundHit(out hit);
+		if (groundedL)
+		{
+			travelL = (-WheelL.transform.InverseTransformPoint(hit.point).y - WheelL.radius) / WheelL.suspensionDistance;
+		}
+
+		bool groundedR = WheelR.GetGroundHit(out hit);
+		if (groundedR)
+		{
+			travelR = (-WheelR.transform.InverseTransformPoint(hit.point).y - WheelR.radius) / WheelR.suspensionDistance;
+		}
+
+		float balanceForce = (travelL - travelR) * balance;
+
+		if (groundedL)
+			car.AddForceAtPosition(WheelL.transform.up * -balanceForce, WheelL.transform.position);
+
+		if (groundedR)
+			car.AddForceAtPosition(WheelR.transform.up * balanceForce, WheelR.transform.position);
+	}
+}
+    
+        
+    
